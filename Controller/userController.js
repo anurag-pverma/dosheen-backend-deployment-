1e8e046e2713bce757c40e0d397fdb80548b335a
Anurag Verma <94588992+anurag-pverma@users.noreply.github.com>
2022-12-20 15:37:56 +0530

    update

--- Controller/userController.js
+++ Controller/userController.js
@@ -26,3 +26,17 @@ export const  createData = async (req, res)=>{
 
 }
 
+export const deleteData = async (req, res)=>{
+    await UserData.deleteOne({_id:req.params.id});
+    res.json({message: "successfull deleted"})
+}
+
+export const  updateData = async (req, res)=>{
+    try {
+        await UserData.updateOne({_id: req.params.id}, {$set: req.body});
+        res.status(200).json({message: "update Successfully"})
+    } catch (error) {
+        res.status(404).json(error.errors)
+    }
+}
+
